cc55060dbf0e84437590c91b0cc2ec679cd58b00
Ahmed Elesawy <ahmedelesawy@Ahmeds-MacBook-Pro-2.local>
2023-07-23 17:12:07 +0300

    PrimeNumCode

---
+++ day08/PrimeNum.py
@@ -0,0 +1,22 @@
+#Write your code below this line 👇
+
+def prime_checker(number):
+    n = number
+    y = n 
+    num_lst = [] 
+    while y >= 1:
+        print(f"{n} % {y}  = {(n) % (y)}")
+        res = n % y
+        y -= 1
+        num_lst.append(res)
+    print(num_lst)
+    indices = [i for i, x in enumerate(num_lst) if x == 0]
+    if(len(indices)> 2):
+        print("It's not a prime number.")
+    else:
+        print("It's a prime number.")    
+#Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+n = int(input("Check this number: "))
+prime_checker(number=n)
---
+++ day08/greet.py
@@ -0,0 +1,27 @@
+
+
+def greet():
+    print('greeting1')
+    print('greeting2')
+    print('greeting3')
+
+greet()
+
+
+name = input("what is your name: ... ? \t ")
+location = input("where is your location .. ? \t ")
+
+def greet_with_name(name, location):
+    print(f"hello {name}")
+    print(f"how are you {name}")
+    print(f"so you are from {location}")
+
+
+greet_with_name(location, name)
+
+
+
+
+
+
+
